313b8f9397e6bfff4ebc59d57f861f08b39c971c
antonia-psomadaki <antonia.psomadaki@vodafone.com>
2023-05-04 14:25:48 +0300

    Top navbar style.

--- src/components/navigation/TopNavbar.tsx
+++ src/components/navigation/TopNavbar.tsx
@@ -1,18 +1,71 @@
 import Button from "../interaction/Button";
 import { Link } from "react-router-dom";
+import { HTMLProps } from "react";
 
+type Props = {
+  color?: "white" | "natural";
+}
 
-const TopNavbar = ({}) => {
+const TopNavbar = ({color}: Props) => {
+
   return (
-    <div className="h-14 primary-500 justify-center w-full flex">
+    <div className={`h-14 primary-500 text-${color} justify-center w-full flex`}>
       <div className="flex justify-between items-center w-3/4">
         Logo
-        <div className="flex"></div>
+        <div className="flex w-1/2 justify-between">
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
+              About Us
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Teammate 1</a>
+              </li>
+              <li>
+                <a>Teammate 2</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
+              Resources
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
+          </div>
+
+
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
+              Pricing
+            </label>
+          </div>
+
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
+              GitHub
+            </label>
+          </div>
+          
+        </div>
         <div>
           <Link to="/login">
-            <Button styles="btn-ghost">Login</Button>
+            <Button styles="btn-ghost mr-2">Login</Button>
           </Link>
-          <Button styles="btn-secondary">Join</Button>
+          <Button styles="btn-secondary px-10">Join</Button>
         </div>
       </div>
     </div>

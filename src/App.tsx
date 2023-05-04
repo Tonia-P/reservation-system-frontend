a943494ca29c76cabe150c8ace4a0a978ad3be34
antonia-psomadaki <antonia.psomadaki@vodafone.com>
2023-05-04 15:20:17 +0300

    TopNavbarTab component, clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {
   Outlet,
-  RouterProvider,
 } from "react-router-dom";
-import Button from './components/interaction/Button'
 import TopNavbar from './components/navigation/TopNavbar'
-import LoginForm from './pages/login/LoginForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <TopNavbar/>
-        <Outlet />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <Outlet />
     </div>
   )
 }
--- src/components/navigation/TopNavbar.tsx
+++ src/components/navigation/TopNavbar.tsx
@@ -1,69 +1,45 @@
 import Button from "../interaction/Button";
 import { Link } from "react-router-dom";
-import { HTMLProps } from "react";
+import { TopNavbarTab } from "./TopNavbarTab";
 
 type Props = {
   color?: "white" | "natural";
-}
+};
 
-const TopNavbar = ({color}: Props) => {
-
+const TopNavbar = ({ color }: Props) => {
   return (
-    <div className={`h-14 primary-500 text-${color} justify-center w-full flex`}>
+    <div
+      className={`h-14 primary-500 text-${color} justify-center w-full flex`}
+    >
       <div className="flex justify-between items-center w-3/4">
-        Logo
+        <label className="normal-case">Logo</label>
+
         <div className="flex w-1/2 justify-between">
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
-              About Us
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Teammate 1</a>
-              </li>
-              <li>
-                <a>Teammate 2</a>
-              </li>
-            </ul>
-          </div>
+          <TopNavbarTab label="About Us" labelcolor={color}>
+            <li>
+              <a>Teammate 1</a>
+            </li>
+            <li>
+              <a>Teammate 2</a>
+            </li>
+          </TopNavbarTab>
 
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
-              Resources
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
-          </div>
+          <TopNavbarTab label="Resources" labelcolor={color}>
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </TopNavbarTab>
 
+          <TopNavbarTab label="Pricing" labelcolor={color} />
 
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
-              Pricing
-            </label>
-          </div>
-
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className={`normal-case ${color} btn btn-ghost m-1`}>
-              GitHub
-            </label>
-          </div>
-          
+          <TopNavbarTab label="GitHub" labelcolor={color} />
         </div>
         <div>
           <Link to="/login">
-            <Button styles="btn-ghost mr-2">Login</Button>
+            <Button styles="btn-ghost mr-3">Login</Button>
           </Link>
           <Button styles="btn-secondary px-10">Join</Button>
         </div>

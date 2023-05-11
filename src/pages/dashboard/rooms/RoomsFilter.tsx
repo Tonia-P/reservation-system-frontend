eca4b043d1f7b6a1072a57078f07c8844cf3a1da
antonia-psomadaki <antonia.psomadaki@vodafone.com>
2023-05-12 02:03:58 +0300

    Layout fix.

---
+++ src/pages/dashboard/rooms/RoomsFilter.tsx
@@ -0,0 +1,41 @@
+export const RoomsFilter = () => {
+  return (
+    <div className="border relative">
+      <div>
+        <div className="dropdown">
+          <label tabIndex={0} className="btn m-1">
+            Click
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="dropdown">
+          <label tabIndex={0} className="btn m-1">
+            Click
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
